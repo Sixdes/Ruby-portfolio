8d71e273d535857e77c5d59fb99db098c9fb30d0
Sixdes <ytxmailg@gmail.com>
2017-07-31 10:29:06 +0800

    Integrated technologies model

--- db/seeds.rb
+++ db/seeds.rb
@@ -65,3 +65,19 @@ end
 end
 
 puts "9 portfolios created"
+
+3.times do |technology|
+	Portfolio.last.technologies.create!(
+		name: "Technology #{technology}"
+		)
+end
+
+
+puts "3 technologies created"
+
+
+
+
+
+
+
